86c5f7d53c071da4a0162ee582f2d9037d487a0e
bjyfzhangqi <bjyfzhangqi@jd.com>
2018-12-04 22:48:27 +0800

    generics chapter 3 tmp commit

--- src/main/java/com/myproc/collections/Generics.java
+++ src/main/java/com/myproc/collections/Generics.java
@@ -1,5 +1,6 @@
 package com.myproc.collections;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -17,8 +18,56 @@ public class Generics {
         }
     }
 
-    public static double sum(Collection<? extends Number> nums) { double s = 0.0;
+    public static double sum(Collection<? extends Number> nums) {
+        double s = 0.0;
         for (Number num : nums) s += num.doubleValue();
         return s;
+    }
+
+    public static <T> List<T> factory() {
+        return new ArrayList<T>();
+    }
+}
+
+abstract class Fruit implements Comparable<Fruit> {
+    protected String name;
+    protected int size;
+
+    protected Fruit(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Fruit) {
+            Fruit that = (Fruit) o;
+            return this.name.equals(that.name) && this.size == that.size;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int compareTo(Fruit that) {
+        return this.size < that.size ? -1 :
+                this.size == that.size ? 0 : 1;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() * 29 + size;
+    }
+}
+
+class Apple extends Fruit {
+    public Apple(int size) {
+        super("Apple", size);
+    }
+}
+
+class Orange extends Fruit {
+    public Orange(int size) {
+        super("Orange", size);
     }
 }
--- src/test/java/com/myproc/collections/GenericsTest.java
+++ src/test/java/com/myproc/collections/GenericsTest.java
@@ -3,9 +3,8 @@ package com.myproc.collections;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class GenericsTest {
 
@@ -24,6 +23,75 @@ public class GenericsTest {
         List<Double> doubles = Arrays.asList(2.78, 3.14);
         Assert.assertTrue(5.92 == Generics.sum(doubles));
         List<Number> nums = Arrays.<Number>asList(1, 2, 2.78, 3.14);
-        Assert.assertTrue(8.92 ==  Generics.sum(nums));
+        Assert.assertTrue(8.92 == Generics.sum(nums));
     }
+
+    @Test
+    public void testInstanceCreation() {
+        List<Number> nums = new ArrayList<Number>();
+        List<? super Number> sink = nums;
+        List<? extends Number> source = nums;
+        for (int i = 0; i < 10; i++) sink.add(i);
+        double sum = 0;
+        for (Number num : source) sum += num.doubleValue();
+        Assert.assertTrue(45.0 == sum);
+        List<Object> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1, 2, 3));
+    }
+
+    @Test
+    public void testNestedWildCards() {
+        List<List<?>> lists = new ArrayList<>();
+        lists.add(Arrays.asList(1, 2, 3));
+        lists.add(Arrays.asList("four", "five"));
+        Assert.assertEquals("[[1, 2, 3], [four, five]]", lists.toString());
+    }
+
+    @Test
+    public void testGenericMethodCall() {
+        List<?> list = Generics.factory();
+        List<?> list2 = Generics.<Object>factory();
+        List<List<?>> list3 = Generics.<List<?>>factory();
+    }
+
+    @Test
+    public void testMaxFruit() {
+        Apple a1 = new Apple(1);
+        Apple a2 = new Apple(2);
+        Orange o3 = new Orange(3);
+        Orange o4 = new Orange(4);
+        List<Apple> apples = Arrays.asList(a1, a2);
+        Assert.assertEquals(a2, Collections.max(apples));
+        List<Orange> oranges = Arrays.asList(o3, o4);
+        Assert.assertEquals(o4, Collections.max(oranges));
+        List<Fruit> mixed = Arrays.<Fruit>asList(a1, o3);
+        Assert.assertEquals(o3, Collections.max(mixed));
+    }
+
+    @Test
+    public void testComparator() {
+        Comparator<String> orderSize =
+                new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return
+                                o1.length() < o2.length() ? -1 :
+                                        o1.length() > o2.length() ? 1 :
+                                                o1.compareTo(o2);
+                    }
+                };
+        Assert.assertTrue("two".compareTo("three") > 0);
+        Assert.assertTrue(orderSize.compare("two", "three") < 0);
+    }
+
+    @Test
+    public void testBridge() {
+        for (Method m : Integer.class.getMethods()
+        ) {
+            if (m.getName().equals("compareTo")) {
+                System.out.println(m.toGenericString());
+            }
+        }
+    }
+
 }
